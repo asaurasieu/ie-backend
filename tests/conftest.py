220f9276eb4752512e92111544efe618b1a18a21
asauras <asauras.ieu2021@student.ie.edu>
2023-10-05 16:34:06 +0200

    requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,13 @@ from iebank_api import db, app
 
 @pytest.fixture
 def testing_client(scope='module'):
-    db.create_all()
-    account = Account('Test Account', '€')
-    db.session.add(account)
-    db.session.commit()
+    with app.app_context():
+        db.create_all()
+        account = Account('Test Account', '€')
+        db.session.add(account)
+        db.session.commit()
 
-    with app.test_client() as testing_client:
-        yield testing_client
+        with app.test_client() as testing_client:
+            yield testing_client
 
-    db.drop_all()
+        db.drop_all()
